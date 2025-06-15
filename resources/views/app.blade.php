0cf5cc82816ba75dd7e2028868af125bba53c0fd
Tazwar Saif <tazwarsaif02@gmail.com>
2025-06-15 12:47:59 +0600

    edit app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+
     @viteReactRefresh
-    @vite(['resources/css/app.css','resources/js/app.jsx'])
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
 </head>
 <body>
